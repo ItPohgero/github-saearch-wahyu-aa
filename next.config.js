5f8b7f9090292a4d02e1900fd3c9edf442edc127
wahyuagusarifin <wahyu.agusarifin@ai.astra.co.id>
2023-06-12 19:39:37 +0700

    update pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/github-saearch-wahyu-aa", // Ubah "github-saearch-wahyu-aa" dengan nama repositori GitHub Anda
+  assetPrefix: "/github-saearch-wahyu-aa/", // Ubah "github-saearch-wahyu-aa" dengan nama repositori GitHub Anda
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
