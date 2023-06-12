c26fc351000a818671385ce42e11b610f4c5c9ce
wahyuagusarifin <wahyu.agusarifin@ai.astra.co.id>
2023-06-12 19:48:35 +0700

    update ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,21 +36,23 @@ const Index = () => {
     }
   };
   return (
-    <div className='max-w-xl mx-auto bg-slate-100 min-h-screen p-4'>
+    <div className='max-w-xl mx-auto bg-slate-100 min-h-screen px-4'>
       <LoadingOverlay visible={loading} overlayBlur={2} />
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <FormInput
-          control={control}
-          name='search'
-          placeholder='Enter username'
-        />
-        <button className='py-3 bg-blue-500 hover:bg-blue-700 text-white uppercase text-sm w-full rounded-lg mt-4 text-center flex justify-center items-center'>
-          Search
-        </button>
-        {getValues('search') && (
-          <div className='my-2 bg-slate-200/50 p-2 rounded-lg'>Showing users for {`"${getValues('search')}"`}</div>
-        )}
-      </form>
+      <div className='sticky top-0 pt-4 pb-2 bg-slate-100'>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormInput
+            control={control}
+            name='search'
+            placeholder='Enter username'
+          />
+          <button className='py-3 bg-blue-500 hover:bg-blue-700 text-white uppercase text-sm w-full rounded-lg mt-4 text-center flex justify-center items-center'>
+            Search
+          </button>
+          {getValues('search') && (
+            <div className='my-2 bg-slate-200/50 p-2 rounded-lg'>Showing users for {`"${getValues('search')}"`}</div>
+          )}
+        </form>
+      </div>
       <Accordion defaultValue="1" transitionDuration={400} onChange={(e: any) => getRepos(e)}>
         {data?.map((dt: any, index: number) => {
           return (
@@ -106,7 +108,7 @@ const Index = () => {
           )
         })}
       </Accordion>
-
+      <div className='fixed bottom-4 left-1/2 -translate-x-1/2 uppercase text-xs bg-slate-300 text-white p-1'>wahyu agus arifin</div>
     </div>
   )
 }
